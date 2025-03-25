5fdd4ea873d7421a51738ab7b77a7a6fa77af1f0
Jason Day <jason@jlogday.com>
2025-03-25 18:24:06 -0400

    Add application.yaml
    - Remove application.properties and add yaml
    - Remove datasource config from app config

--- src/main/java/com/jlogday/recipe/RecipeApplicationConfig.java
+++ src/main/java/com/jlogday/recipe/RecipeApplicationConfig.java
@@ -1,22 +1,8 @@
 package com.jlogday.recipe;
 
-import javax.sql.DataSource;
-
-import org.springframework.boot.jdbc.DataSourceBuilder;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RecipeApplicationConfig {
 
-	@Bean
-	public DataSource getDataSource() {
-		var builder = DataSourceBuilder.create();
-		builder.driverClassName("org.postgresql.Driver");
-		builder.url("jdbc:postgresql://localhost:51846/postgres");
-		builder.username("myuser");
-		builder.password("secret");
-		
-		return builder.build();
-	}
 }
