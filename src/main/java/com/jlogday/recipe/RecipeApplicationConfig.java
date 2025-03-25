b55a524f1aaa80f9dc39b67504c549a9c287d02c
Jason Day <jason@jlogday.com>
2025-03-25 14:11:58 -0400

    Rename config

--- src/main/java/com/jlogday/recipe/RecipeApplicationConfig.java
+++ src/main/java/com/jlogday/recipe/RecipeApplicationConfig.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class DataSourceConfig {
+public class RecipeApplicationConfig {
 
 	@Bean
 	public DataSource getDataSource() {
