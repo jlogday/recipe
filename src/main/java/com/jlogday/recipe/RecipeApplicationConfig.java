4cd2befc16d252b38852606958e7171fc60873c3
Jason Day <jason@jlogday.com>
2025-04-08 16:51:22 -0400

    Add LocationHandler and CorsConfiguration
    - Add LocationHandler for testing angular integration
    - Add CORS configuration to allow REST requests from angular

--- src/main/java/com/jlogday/recipe/RecipeApplicationConfig.java
+++ src/main/java/com/jlogday/recipe/RecipeApplicationConfig.java
@@ -5,10 +5,14 @@ import static org.springframework.web.reactive.function.server.RequestPredicates
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
+import com.jlogday.recipe.handler.LocationHandler;
 import com.jlogday.recipe.handler.RecipeHandler;
 
 @Configuration
@@ -21,4 +25,32 @@ public class RecipeApplicationConfig {
                 .GET("/recipes/{id}", accept(MediaType.APPLICATION_JSON), handler::getRecipeById)
                 .build();
     }
+
+    // for testing angular integration
+    @Bean
+    public RouterFunction<ServerResponse> locationsRoute(LocationHandler handler) {
+    return RouterFunctions.route()
+            .GET("/locations", accept(MediaType.APPLICATION_JSON), handler::getAllLocations)
+            .GET("/locations/{id}", accept(MediaType.APPLICATION_JSON), handler::getLocationById)
+            .build();
+    }
+
+    @Bean
+    public CorsWebFilter corsFilter() {
+
+        var config = new CorsConfiguration();
+
+        config.applyPermitDefaultValues();
+
+        //config.setAllowCredentials(true);
+        config.addAllowedOrigin("http://localhost:4200");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+
+        var source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsWebFilter(source);
+    }
+
 }
