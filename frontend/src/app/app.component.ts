8ab2b9922d29a6c6572f0300ca8c62602b4910d8
Jason Day <jason@jlogday.com>
2025-04-30 11:31:49 -0400

    Remove import to fix warning message

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -5,7 +5,7 @@ import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterModule, HomeComponent],
+  imports: [RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
