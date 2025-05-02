ee03076c65c7ac30bde67f1281aa2420a3431002
Jason Day <jason@jlogday.com>
2025-05-02 18:28:26 -0400

    Add instructions to form
    - Refactor recipe-form to use a FormBuilder and add arrays for the
      instructions and ingredients (ingredients WIP)
    - Add the new elements to the html
    - Add the form value to the html for debugging

--- frontend/src/app/recipe-form/recipe-form.component.ts
+++ frontend/src/app/recipe-form/recipe-form.component.ts
@@ -2,11 +2,12 @@ import { Component, inject } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Recipe } from '../recipe';
 import { RecipeService } from '../recipe.service';
-import { FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
+import { FormArray, FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-recipe-form',
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, JsonPipe],
   templateUrl: './recipe-form.component.html',
   styleUrl: './recipe-form.component.css'
 })
@@ -14,13 +15,33 @@ export class RecipeFormComponent {
   route: ActivatedRoute = inject(ActivatedRoute);
   router = inject(Router);
   recipeService = inject(RecipeService);
+  formBuilder = inject(FormBuilder);
 
-  newRecipeForm = new FormGroup({
-    name: new FormControl(''),
-    description: new FormControl(''),
-  });
+  newRecipeForm = this.formBuilder.group({
+    name: [''],
+    description: [''],
+    photo: [''],
+    instructions: this.formBuilder.array([this.formBuilder.control('')]),
+    ingredients: this.formBuilder.array([this.formBuilder.control('')]),
+  })
 
   constructor() {
+  }
+
+  get instructions() {
+    return this.newRecipeForm.get('instructions') as FormArray;
+  }
+
+  addInstruction() {
+    this.instructions.push(this.formBuilder.control(''));
+  }
+
+  get ingredients() {
+    return this.newRecipeForm.get('ingredients') as FormArray;
+  }
+
+  addIngredient() {
+    this.instructions.push(this.formBuilder.control(''));
   }
 
 
